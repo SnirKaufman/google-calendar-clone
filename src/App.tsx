19e6ed7774adad358c10da161641779a1a53fba4
snir <snirkaufman4@gmail.com>
2023-12-28 13:31:15 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Calendar } from "./components/Calendar";
+import "./styles.css";
+
+function App() {
+  return <Calendar />;
+}
+
+export default App;
---
+++ src/components/Calendar.tsx
@@ -0,0 +1,102 @@
+import { useMemo, useState } from "react";
+import {
+  startOfWeek,
+  startOfMonth,
+  endOfWeek,
+  endOfMonth,
+  eachDayOfInterval,
+  isSameMonth,
+  isBefore,
+  endOfDay,
+  isToday,
+} from "date-fns";
+import { formatDate } from "../utils/formatDate";
+import { cc } from "../utils/cc";
+
+export function Calendar() {
+  const [selectedMonth, setSelectedMonth] = useState(new Date());
+
+  const calendarDays = useMemo(() => {
+    const firstWeekStart = startOfWeek(startOfMonth(selectedMonth));
+    const lastWeekEnd = endOfWeek(endOfMonth(selectedMonth));
+    return eachDayOfInterval({ start: firstWeekStart, end: lastWeekEnd });
+  }, [selectedMonth]);
+
+  return (
+    <div className="calendar">
+      <div className="header">
+        <button
+          className="btn"
+          onClick={() => {
+            setSelectedMonth(new Date());
+            console.log(selectedMonth);
+          }}
+        >
+          Today
+        </button>
+        <div>
+          <button className="month-change-btn">&lt;</button>
+          <button className="month-change-btn">&gt;</button>
+        </div>
+        <span className="month-title">June 2023</span>
+      </div>
+      <div className="days">
+        {calendarDays.map((day, index) => {
+          return (
+            <CalendarDay
+              key={day.getTime()}
+              day={day}
+              showWeekName={index < 7}
+              selectedMonth={selectedMonth}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+type CalendarDayProps = {
+  day: Date;
+  showWeekName: boolean;
+  selectedMonth: Date;
+};
+
+function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
+  return (
+    <div
+      className={cc(
+        "day",
+        !isSameMonth(day, selectedMonth) && "non-month-day",
+        isBefore(endOfDay(day), new Date()) && "old-month-day"
+      )}
+    >
+      <div className="day-header">
+        {showWeekName && (
+          <div className="week-name">
+            {formatDate(day, { weekday: "short" })}
+          </div>
+        )}
+        <div className={cc("day-number", isToday(day) && "today")}>
+          {formatDate(day, { day: "numeric" })}
+        </div>
+        <button className="add-event-btn">+</button>
+      </div>
+      {/* <div className="events">
+        <button className="all-day-event blue event">
+          <div className="event-name">Short</div>
+        </button>
+        <button className="all-day-event green event">
+          <div className="event-name">
+            Long Event Name That Just Keeps Going
+          </div>
+        </button>
+        <button className="event">
+          <div className="color-dot blue"></div>
+          <div className="event-time">7am</div>
+          <div className="event-name">Event Name</div>
+        </button>
+      </div> */}
+    </div>
+  );
+}
